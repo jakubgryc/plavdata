24af4479a12b6b27e162060ab82ed7fcc83752b1
Jakub Gryc <grycjakub00@gmail.com>
2025-08-25 21:25:14 +0200

    refactor: remove unused PostgreSQL env variables

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,14 +1,6 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DB_HOST = os.getenv("POSTGRES_HOST", "db")
-DB_NAME = os.getenv("POSTGRES_DB", "plavdata")
-DB_USER = os.getenv("POSTGRES_USER", "postgres")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-
-# DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
 DATABASE_URL = "sqlite:///./data/sql_app.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
