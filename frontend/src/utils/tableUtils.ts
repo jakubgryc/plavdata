8642df32b60bfbf6e42419e6f6ab17ea158d7c19
Jakub Gryc <grycjakub00@gmail.com>
2025-12-02 22:46:56 +0100

    feat: add modal for additional personal bests information

--- frontend/src/utils/tableUtils.ts
+++ frontend/src/utils/tableUtils.ts
@@ -2,9 +2,39 @@ import type { SwimmerPersonalBest } from "../schema/types.ts";
 import { DISCIPLINES } from "./constants.ts";
 import { parseTimeFromMillis } from "./timeUtils.ts";
 
-type TableRow = Record<string, string>;
+// create a type for discipline description
+// it will be inside the TableRow type
+// like  { "100 K": "1:04.23",
+//         "100 K_description": {
+//           date: "2023-05-12",
+//           split_time: true|false,
+//           relay_part: true|false,
+//           },
+//
+//         ...
+//         }
+//
+
+type DisciplineDescription = {
+  time: string;
+  date: string | undefined;
+  isSplit: boolean | undefined;
+  isRelayPart: boolean | undefined;
+  location: string | undefined;
+  points: number | undefined;
+};
+
+type TableRow = Record<string, string | DisciplineDescription>;
 
 export function buildTableData(data: SwimmerPersonalBest[]): TableRow[] {
+  const emptyDescription: DisciplineDescription = {
+    time: "",
+    date: "",
+    isSplit: false,
+    isRelayPart: false,
+    location: "",
+    points: undefined,
+  };
   return data.map((swimmerBests) => {
     const row: TableRow = {
       name: `${swimmerBests.swimmer.surname} ${swimmerBests.swimmer.name}`,
@@ -13,7 +43,20 @@ export function buildTableData(data: SwimmerPersonalBest[]): TableRow[] {
       const pb = swimmerBests.personal_bests.find(
         (p) => p.discipline?.code === discipline,
       );
+      let pbDescription: DisciplineDescription = emptyDescription;
+      if (pb) {
+        pbDescription = {
+          time: parseTimeFromMillis(pb.time),
+          date: pb.date,
+          isSplit: pb.split_time,
+          isRelayPart: pb.relay_part,
+          location: pb.competition_location,
+          points: pb.points,
+        };
+      }
+
       row[discipline] = pb ? parseTimeFromMillis(pb.time) : "";
+      row[`${discipline}_description`] = pbDescription;
     }
     return row;
   });
--- frontend/src/utils/timeUtils.ts
+++ frontend/src/utils/timeUtils.ts
@@ -9,7 +9,15 @@ export const parseTimeFromMillis = (ms: number): string => {
   return `${minutes}:${paddedSeconds}.${paddedMillis}`;
 };
 
-export function formatDateFromString(timestampMs: number): string {
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}.${month}.${year}`;
+}
+
+export function formatDateFromMs(timestampMs: number): string {
   const date = new Date(timestampMs);
   const day = String(date.getDate()).padStart(2, "0");
   const month = String(date.getMonth() + 1).padStart(2, "0");
