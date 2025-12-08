03dec323fd6058cd7a01632e016c207bf7e0674c
Jakub Gryc <grycjakub00@gmail.com>
2025-12-08 01:34:20 +0100

    fix: db url

--- backend/app/db.py
+++ backend/app/db.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./data/plavdata.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////app/data/database.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
